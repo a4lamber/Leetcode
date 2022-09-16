996556f8b0a171e29fabfbb29451444f7df151e5
Adam Zhang <adamhimy19@gmail.com>
2022-09-16 10:23:41 +0800

    update graph for data structure

---
+++ python-sort/basics/node.py
@@ -0,0 +1,15 @@
+'''
+ # @ Author: Yixiang Zhang
+ # @ Create Time: 2022-09-16 09:46:31
+ # @ Modified by: Yixiang Zhang
+ # @ Modified time: 2022-09-16 10:00:49
+ # @ Description: implementaion of singly linked node class
+ '''
+
+class Node(object):
+    """Implemently of node"""
+    def __init__(self,data,next = None):
+        self.data = data
+        self.next = next
+
+
